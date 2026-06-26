82c7f873465ad3e32df265428b472b1e70b6eec3
Leiziane Trevisan Dardin <leiziane.trevisan@yahoo.com.br>
2026-06-25 18:26:59 -0600

    worked on Search for address fetchign API + finishing Add Property Card

--- app/type/properties.ts
+++ app/type/properties.ts
@@ -3,6 +3,11 @@ export type Property ={
     owner_id: string,
     name: string,
     description: string | null,
+    address: string | null,
+    neighborhood: string | null,
+    square_feet: number | null,
+    parking: boolean,
+    public_transportation: boolean,
     image_url: string | null,
     created_at: string
 };
--- components/AddPropertyModal.tsx
+++ components/AddPropertyModal.tsx
@@ -4,7 +4,6 @@ import { useState } from "react"
 import { supabase } from "@/lib/supabase"
 import { Property } from "@/app/type/properties"
 
-
 type AddPropertyModalProps ={
     onClose: () => void;
     onSaved: () => void;
@@ -17,15 +16,23 @@ export default function AddPropertyModal( { onClose, onSaved, property} : AddPro
     const [ imageFile, setImageFile] = useState<File | null>(null);
     const [ loading, setLoading] = useState(false);
     const [ successMessage, setSuccessMessage] = useState("");
+    const [ address, setAddress ] = useState(property?.address ?? "");
+    const [ neighborhood, setNeighborhood ] = useState(property?.neighborhood ?? "");
+    const [ squareFeet, setSquareFeet ] = useState(property?.square_feet ?? "");
+    const [ parking, setParking ] = useState(property?.parking ?? false);
+    const [ publicTransportation, setPublicTransportation ] = useState(property?.public_transportation ?? false);
+    const [ suggestions, setSuggestions ] = useState([]);//list
+    const [ loadingSuggestions, setLoadingSuggestions ] = useState(false);
+
 
     const handleSubmit = async () => {
     setLoading(true);
+
     const { data: { user } } = await supabase.auth.getUser();
     if (!user) { setLoading(false); return; }
 
-    let image_url = property?.image_url ?? ""; // mantém a imagem antiga se não trocar
+    let image_url = property?.image_url ?? ""; 
 
-    // só faz upload se escolheu nova imagem
     if (imageFile) {
         const fileExt = imageFile.name.split(".").pop();
         const fileName = `${user.id}-${Date.now()}.${fileExt}`;
@@ -44,23 +51,24 @@ export default function AddPropertyModal( { onClose, onSaved, property} : AddPro
     }
 
     if (property) {
-      
         const { error } = await supabase
             .from("properties")
-            .update({ name, description, image_url })
-            .eq("id", property.id);
+            .update({ name, description, image_url, address, neighborhood, square_feet: squareFeet ? Number(squareFeet) : null, parking, public_transportation: publicTransportation})
+            .eq("id", property.id); 
 
             console.log("update error:", error); 
-    console.log("update id:", property.id); 
-
-        if (error) { console.error(error); setLoading(false); return; }
-    } else {
+            console.log("update id:", property.id);
+        if (error) { 
+            console.error(error); 
+            setLoading(false);
+            return;} 
         
-        if (!imageFile) { setLoading(false); return; }
+        }else{
+            
 
         const { error } = await supabase
             .from("properties")
-            .insert({ owner_id: user.id, name, description, image_url });
+            .insert({ owner_id: user.id, name, description, image_url, address, neighborhood, square_feet: squareFeet ? Number(squareFeet) : null, parking, public_transportation: publicTransportation});
 
         if (error) { console.error(error); setLoading(false); return; }
     }
@@ -69,6 +77,28 @@ export default function AddPropertyModal( { onClose, onSaved, property} : AddPro
     onSaved();
     setLoading(false);
     setTimeout(() => onClose(), 1000);
+};
+
+const searchAddress = async () => {
+    if(!address) return;
+    setLoadingSuggestions(true);
+
+    const response = await fetch(
+        `https://nominatim.openstreetmap.org/search?q=${encodeURIComponent(address)}&format=json&addressdetails=1&limit=5&countrycodes=ca`,
+        { headers: {"Accept-Language": "en"}}
+    );
+
+    const data = await response.json();
+    setSuggestions(data);
+    setLoadingSuggestions(false);
+
+};
+
+const handleSelectSuggestions = (suggestion: any) => {
+    setAddress(suggestion.display_name);
+    setNeighborhood(suggestion.address.suburb ?? suggestion.address.village ?? "")
+    setSuggestions([])
+
 };
 
 return (
@@ -95,20 +125,89 @@ return (
                 value={name}
                 onChange={(e) => setName(e.target.value)}
             />
-            
+
+            <div className="border border-gray-200 rounded-md p-2 mt-10 text-gray-500 shadow-sm">
+                <input
+                type="text"
+                placeholder="Search Address"
+                value={address}
+                onChange={(e) => setAddress(e.target.value)}
+                autoComplete="on"
+                className="w-full outline-none text-gray-500">
+                </input>
+</div>
+                <button onClick={searchAddress} disabled={loading}
+            className="w-full py-2 rounded px-3 bg-gradient-to-r from-blue-500 to-purple-600 text-white font-semibold hover:from-blue-600 hover:to-purple-700 transition-colors mt-5"
+            >
+                Search
+            </button>
+            {suggestions.length > 0 && (
+                <ul className="border border-gray-200 rounded-md shadow-sm">
+                    {suggestions.map((suggestion: any, index) => (
+                    <li
+                        key={index}
+                        onClick={() =>
+                        handleSelectSuggestions(suggestion) }
+                        className="p-2 hover:bg-gray-100 cursor-pointer text-sm text-gray-700"
+                        >
+                            {suggestion.display_name}
+                        </li>
+                    ))}
+                </ul>
+            )}
+           
+           
+            <input
+                className="text-sm border-gray-200 w-full border border-gray-200 rounded-md mt-5 p-2"
+                type="text"
+                placeholder="Neighborhood"
+                value={neighborhood}
+                onChange={(e) => setNeighborhood(e.target.value)}
+            />
+
+             <input
+                className="text-sm border-gray-200 w-full border border-gray-200 rounded-md mt-5 p-2"
+                type="number"
+                placeholder="Square Feet"
+                value={squareFeet}
+                onChange={(e) => setSquareFeet(e.target.value)}
+            />
+                        
             <textarea
-                className="text-base mb-5 border-gray-200 w-full border border-gray-200 rounded-md mt-5 p-2"
+                className="text-base mb-1 border-gray-200 w-full border border-gray-200 rounded-md mt-5 p-2"
                 placeholder="Description"
                 value={description}
                 onChange={(e)=> setDescription(e.target.value)}
             />
-
-            <input
-                className="text-sm  border-gray-200 w-full border border-gray-200 rounded-md mt-5 p-2"
-                type="file"
-                accept="image/*"
-                onChange={(e) => setImageFile(e.target.files?.[0] ?? null )}
+            <div className="flex items-center gap-6 mt-5 mb-5">
+                        <label className="flex items-center gap-2 mt-5 m text-sm text-gray-700">
+                            <input
+                                type="checkbox"
+                                checked={parking}
+                                onChange={(e) => setParking(e.target.checked)}
+                                className="accent-purple-600 w-4 h-4"
+                                />
+                                Parking
+                        </label>
+                        
+                        <label className="flex items-center gap-2 mt-5 text-sm text-gray-700">
+                            <input
+                                type="checkbox"
+                                checked={publicTransportation}
+                                onChange={(e) => setPublicTransportation(e.target.checked)}
+                                className="accent-purple-600 w-4 h-4" 
+                                />
+                                Public Transportation
+                        </label>
+            </div>
+                        
+               <input
+                    className="text-sm  border-gray-200 w-full border border-gray-200 rounded-md mt-5 p-2"
+                    type="file"
+                    accept="image/*"
+                    onChange={(e) => setImageFile(e.target.files?.[0] ?? null )}
             />
+            
 
             <button onClick={handleSubmit} disabled={loading}
             className="w-full py-2 rounded px-3 bg-gradient-to-r from-blue-500 to-purple-600 text-white font-semibold hover:from-blue-600 hover:to-purple-700 transition-colors mt-5">
